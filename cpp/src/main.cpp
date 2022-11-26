157dea4efe314d95f569f87b5b5fd019b8415d98
knguyen1401 <79705826+knguyen1401@users.noreply.github.com>
2022-11-26 09:48:20 +0700

    Update main.cpp

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -11,6 +11,51 @@ void clear(){
     #endif
 }
 
+void reset_cin(){
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    clear();
+}
+
+void getDec(unsigned long long a){
+    int len = floor(log10(a)) + 1; //Number lenght
+    vector<int> list; //For storing digits
+    int result = 0;
+
+    for (int i = 0; i < len; i++){
+        if (a%10==1 || a%10 ==0){
+            list.push_back(a%10);
+        }
+        else{
+            cout<<RED<<"Binary cannot contain " << a%10 << NORM << endl;
+            return;
+        }
+        a/=10;
+    }
+    reverse(list.begin(), list.end());
+    for (int x : list){
+        result = result + x*(pow(2, len-1));
+        len--;
+    }
+    /*
+    For example, we have 1000101 as a binary code, and we want to convert it to decimal,
+    we will do this
+
+    2^6*1
+    2^5*0
+    2^4*0
+    2^3*0
+    2^2*1
+    2^1*0
+    2^0*1
+
+    and the result is the sum of the numbers above.
+    */
+    cout << GREEN << "Result: " <<result<< NORM <<endl;
+    list.clear();
+}
+
 void getBin(unsigned long long a){
     vector<int> list;
 
@@ -37,6 +82,7 @@ void getBin(unsigned long long a){
 int main(){
     unsigned long long a;
     string b;
+    int choice;
 
     #ifdef _WIN32
     WinEnableColor();
@@ -54,22 +100,45 @@ int main(){
         cout << "\"Ctrl+Q\"" << endl << endl;
         #endif
 
-        cout << "Enter an interger to get the binary value: ";
-
-        if (!(cin >> a)){
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-            clear();
+        cout << "Select one option:" << endl << "1. Decimal -> Binary" << endl << "2. Binary -> Decimal" << endl;
+        cout<<"Selection: ";
+        if(!(cin >> choice) || choice >=3){
+            reset_cin();
             continue;
-
-            /*Input check whether the input is an interger or not.
-            If not it will ignore and reset the screen*/
         }
         else{
-            getBin(a);
+            if (choice == 1){
+                cout << "Enter an integer to get the binary value: ";
 
-            cout << "\n";
+                if (!(cin >> a)){
+                    reset_cin();
+                    continue;
+
+                    /*Input check whether the input is an integer or not.
+                    If not it will ignore and reset the screen*/
+                }
+                else{
+                    getBin(a);
+
+                    cout << "\n";
+                }
+            }
+            else if (choice ==2){
+                cout << "Enter binary to get integer: ";
+
+                if (!(cin >> a)){
+                    reset_cin();
+                    continue;
+
+                    /*Input check whether the input is an integer or not.
+                    If not it will ignore and reset the screen*/
+                }
+                else{
+                    getDec(a);
+
+                    cout << "\n";
+                }
+            }
         }
 
         cout << "Enter anything to clear: ";
